4ff4b2d19866a2437ef07674603d3e9f610e6dc8
anujpapriwal <30831825+anujpapriwal@users.noreply.github.com>
2019-03-05 14:27:21 +0530

    added helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var hbs = require("express-handlebars");
+var helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -30,6 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static("public"));
+app.use(helmet());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
